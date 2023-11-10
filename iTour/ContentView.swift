4b3f514c23c226cbc12dba3614e92c339931f44e
Tal Spektor <tal.s@zemingo.com>
2023-11-10 18:47:16 +0200

    base

--- iTour/ContentView.swift
+++ iTour/ContentView.swift
@@ -5,17 +5,39 @@
 //  Created by Tal talspektor on 10/11/2023.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var destinations: [Destination]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(destinations) { destination in
+                    Text(destination.name)
+                        .font(.headline)
+
+                    Text(destination.date.formatted(date: .long, time: .shortened))
+                }
+            }
+            .navigationTitle("iTour")
+            .toolbar {
+                Button("Add Samples", action: addSamples)
+            }
         }
-        .padding()
+
+    }
+
+    func addSamples() {
+        let rome = Destination(name: "Rome")
+        let florence = Destination(name: "Florence")
+        let naples = Destination(name: "Naples")
+
+        modelContext.insert(rome)
+        modelContext.insert(florence)
+        modelContext.insert(naples)
     }
 }
 
--- iTour/iTourApp.swift
+++ iTour/iTourApp.swift
@@ -5,6 +5,7 @@
 //  Created by Tal talspektor on 10/11/2023.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct iTourApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Destination.self)
     }
 }
