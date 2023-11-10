1679d6fd3cc7d6fb26e0114ce927e1cc7dc2963d
Tal Spektor <tal.s@zemingo.com>
2023-11-10 17:12:12 +0200

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Tal talspektor on 10/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
